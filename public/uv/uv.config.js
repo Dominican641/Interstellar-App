1f7a5913b410ddc97091e536d4472110e23c055d
n0ob3 <93485553+Dominican641@users.noreply.github.com>
2023-06-01 23:03:55 -0400

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -2,7 +2,7 @@
 
 self.__uv$config = {
   prefix: "/uv/service/",
-  bare: "https://hydrometeorology.newnotionscience.com/bare/",
+  bare: "/bare/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
