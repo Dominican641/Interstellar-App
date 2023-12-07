74b5ee5584048ce74b51a8361cbeb0c46d20d713
3730 <93485553+Dominican641@users.noreply.github.com>
2023-12-07 01:10:23 +0000

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
   prefix: "/uv/service/",
-  bare: "https://tribe.norushcharge.com/bare/",
+  bare: "https://server.flow-works.me/bare/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/uv/uv.handler.js",
